54c3411dc1920eda674e89020adf3ea2ba5a8364
seungho Jeon <urijsh612@gmail.com>
2020-01-15 17:18:22 +0900

    #3.2 인터페이스 특성과 type alias

--- scissorsRockPaper/SRP.ts
+++ scissorsRockPaper/SRP.ts
@@ -4,13 +4,32 @@ interface RSP {
   PAPER: "-284px";
 }
 
+// 1. type alias로 작성시
+// type RSP = {
+//   ROCK: "0";
+//   SCISSORS: "-142px";
+//   PAPER: "-284px";
+// }
+
+// // 2. 인터페이스의 새로올 인자 틀 만들어 놓기(왠만하면 안쓰는게 좋다)
+// interface Example {
+//   a: 3,
+//   b: 4,
+//   [key: string] : number // 나중에 들어올 값이 확실치 않아 틀정도만 정해두고 자 할 때
+// }
+// const example: Example = {
+//   a: 3,
+//   b: 4,
+//   z: 16
+// }
+
 const rsp: RSP = {
   ROCK: "0",
   SCISSORS: "-142px",
   PAPER: "-284px"
 };
 
-let imgCoords: RSP[keyof RSP] = "0";
+let imgCoords: RSP[keyof RSP] = "0"; // RSP의 value 사용법
 
 const score = {
   ROCK: 1,
@@ -18,7 +37,7 @@ const score = {
   PAPER: -1
 } as const; // 상수화시키기
 
-function computerChoice(imgCoords) {
+function computerChoice(imgCoords): keyof RSP {
   return Object.keys(rsp).find(key => rsp[key] === imgCoords);
 }
 
