4da2dab31342beecfe36ea1ff9c2d9ad8ca0b38e
seungho Jeon <urijsh612@gmail.com>
2020-01-15 18:02:47 +0900

    #3-4. this와 타입 범위 이해

--- scissorsRockPaper/SRP.ts
+++ scissorsRockPaper/SRP.ts
@@ -29,7 +29,7 @@ const rsp: RSP = {
   PAPER: "-284px"
 };
 
-let imgCoords: RSP[keyof RSP] = "0"; // RSP의 value 사용법
+let imgCoords: RSP[keyof RSP] = "0"; // RSP의 value 사용법(JS의 객체 사용법과 비슷)
 
 const score = {
   ROCK: 1,
@@ -37,17 +37,40 @@ const score = {
   PAPER: -1
 } as const; // 상수화시키기
 
+let interval: number;
+function intervalMaker() {
+  interval = setInterval(function() {
+    if (imgCoords === rsp.ROCK) {
+      imgCoords = rsp.SCISSORS;
+    } else if (imgCoords === rsp.SCISSORS) {
+      imgCoords = rsp.PAPER;
+    } else {
+      imgCoords = rsp.ROCK;
+    }
+    if (document.querySelector("#computer")) {
+      (document.querySelector("#computer") as HTMLElement).style.background =
+        "url(https://en.pimg.jp/023/182/267/1/23182267.jpg) " +
+        imgCoords +
+        " 0";
+    }
+  }, 100);
+}
+
+intervalMaker();
+
 function computerChoice(imgCoords: RSP[keyof RSP]) {
+  //as 를 이용해 Object.keys(rsp)의 값을 좁혀준다.
   return (Object.keys(rsp) as ["ROCK", "SCISSORS", "PAPER"]).find(
     key => rsp[key] === imgCoords
   );
 }
 
 document.querySelectorAll(".btn").forEach(btn => {
-  btn.addEventListener("click", function() {
-    const myChoice = this.textContent;
+  btn.addEventListener("click", function(this: HTMLButtonElement) {
+    //this 값을 명시적으로 넣어 줘야한다.
+    const myChoice = this.textContent as keyof RSP; //as 활용하여 값의 범위를 좁혀준다.
     const myScore = score[myChoice];
-    const computer = score[computerChoice(imgCoords)];
+    const computerScore = score[computerChoice(imgCoords)!]; //느낌표 사용: undefined, null이 아님을 개발자가 보장
     const diff = myScore - computerScore;
     if (diff === 0) {
       console.log("비겼습니다");
